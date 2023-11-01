e9e72d770aef7adb2756fc9ea4db97bd2f76bfd7
Nick Monteleone <nicholasmonteleone25@gmail.com>
2023-10-31 20:57:05 -0700

    reset game works

--- memory-game.js
+++ memory-game.js
@@ -5,7 +5,7 @@ const startButton = document.getElementById("start-button");
 const startBox = document.getElementById("start-box");
 const startText = document.getElementById("start-text");
 const gameBox = document.getElementById("game");
-const endBox = document.getElementById("end-box")
+const endBox = document.getElementById("end-box");
 startButton.addEventListener('click', startGame);
 
 // constants for wait time and number of items
@@ -20,16 +20,16 @@ let matchesRemaining;
 
 /** Memory game: find matching pairs of cards and flip both of them. */
 function startGame() {
-  console.log('starting game');
+  // clear game element to allow for second play
+  for (let i = 0; gameBox.childNodes.length > 0; i++){
+    gameBox.childNodes[0].remove();
+  }
   // decide which images will be in the game
   const imageOptions = chooseImages(IMAGE_FILES_COUNT, IMAGES_TO_MATCH);
-  console.log(`Selected images: ${imageOptions}`);
   // shuffle the images so they are mixed for matching
   const shuffledImages = shuffle(imageOptions);
-  console.log(`Shuffled images: ${shuffledImages}`);
   // create cards from the shuffled images
   createCards(shuffledImages);
-  console.log("set cards");
   // show and hide boxes to start
   gameBox.style.display = "flex";
   startText.style.display = "flex";
@@ -49,7 +49,6 @@ function chooseImages(options, selections) {
   }
   return selectedImages;
 }
-
 
 /** Shuffle array items in-place and return shuffled array. */
 function shuffle(items) {
@@ -107,10 +106,6 @@ function flipCard(id, imageNumber) {
   // flip the card by switching visibility of button and image
   let buttonId = `option-${id}`;
   let resultId = `result-${id}`;
-  console.log('button clicked');
-  console.log(buttonId);
-  console.log(resultId);
-  console.log(imageNumber);
   // show the result and hide the button
   const buttonElement = document.getElementById(buttonId);
   buttonElement.style.display = 'none';
@@ -121,31 +116,27 @@ function flipCard(id, imageNumber) {
   flippedCardImages.push(imageNumber);
   // once two cards flipped, wait set time and check for match
   if (flippedCardIds.length === 2) {
-    setTimeout(checkForMatch, FOUND_MATCH_WAIT_MSECS);
-  }
-}
-
-/** Flip a card face-down. */
-function unFlipCard(id) {
-  // hide the result and show the button
-  document.getElementById(`option-${id}`).style.display = 'flex';
-  document.getElementById(`result-${id}`).style.display = 'none';
-}
-
-/* check for match when two cards are flipped */
-function checkForMatch() {
-  console.log('checking for match');
-  console.log(flippedCardImages);
-  // if not a match then unflip the cards
-  if (flippedCardImages[0] !== flippedCardImages[1]) {
-    for (let flippedId of flippedCardIds) {
-      unFlipCard(flippedId);
+    if (flippedCardImages[0] == flippedCardImages[1]) {
+      // subtract from matches remaining and check for victory
+      matchesRemaining--;
+      if (matchesRemaining === 0) gameCompletion();
+      // clear id and image arrays
+      flippedCardIds = [];
+      flippedCardImages = [];
     }
+    else {
+      // flip cards after wait time
+      setTimeout(unFlipCards, FOUND_MATCH_WAIT_MSECS);
+    }
+
   }
-  else {
-    // subtract from matches remaining and check for victory
-    matchesRemaining--;
-    if (matchesRemaining === 0) gameCompletion();
+}
+
+/* unflip cards by reversing visibility of image and button */
+function unFlipCards() {
+  for (let id of flippedCardIds) {
+    document.getElementById(`option-${id}`).style.display = 'flex';
+    document.getElementById(`result-${id}`).style.display = 'none';
   }
   // clear id and image arrays
   flippedCardIds = [];
@@ -153,7 +144,7 @@ function checkForMatch() {
 }
 
 /* set up the game for the next play upon completion */
-function gameCompletion(){
+function gameCompletion() {
   startBox.style.display = "flex";
   startText.style.display = "none";
   endBox.style.display = "flex";
